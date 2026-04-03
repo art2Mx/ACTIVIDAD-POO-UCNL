4ff8955dec76dce783c3eac5bb4367afd7566653
art2Mx <artwo.dev@gmail.com>
2026-04-02 21:15:54 -0600

    Update Main.java
    ACTIVIDAD 4

--- Main.java
+++ Main.java
@@ -4,12 +4,34 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        postre p1 = new postre(sc);
-        postre p2 = new postre(sc);
+        System.out.println("¿Cuantos postres hay en el menú? (número)");
+        int cantidad = sc.nextInt();
 
-        p1.anuncio();
-        p2.anuncio();
+        sc.nextLine();
 
-        sc.close();
+        //Polimorfismo
+        Postre[] p = new Postre[cantidad];
+
+        for(int i = 0; i < cantidad; i++) {
+            System.out.println("¿Qué tipo de postre es el postre numero " + (i + 1) + "? Pastel o Gelatina");
+            String tipo = sc.nextLine();
+
+            if (tipo.equalsIgnoreCase("Gelatina")) {
+                p[i] = new Gelatina(sc);
+            }
+            else if (tipo.equalsIgnoreCase("Pastel")) {
+                p[i] = new Pastel(sc);
+            }
+            else {
+                System.out.println("No fue válido el dato, ingresar nuevamente");
+                i--;
+            }
+        }
+
+        System.out.println("--==::POSTRES::==--");
+        for (int i = 0; i < cantidad; i++) {
+           p[i].anuncio();
+           System.out.println("===================");
+        }
     }
 }
